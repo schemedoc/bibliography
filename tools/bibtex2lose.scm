023038da2a94909d2fd1785f37f02a351ecaa59b
Lassi Kortela <lassi@lassi.io>
2020-03-03 23:12:00 +0200

    Parse abstract from any lines after BibTeX

--- tools/bibtex2lose.scm
+++ tools/bibtex2lose.scm
@@ -1,9 +1,18 @@
 #! /usr/bin/env gosh
 
+(import (scheme base) (scheme write) (gauche base) (srfi 13))
+
 (define (read-all-lines)
   (let loop ((lines '()))
     (let ((line (read-line)))
       (if (eof-object? line) (reverse lines) (loop (cons line lines))))))
+
+(define (assert-ascii string)
+  (define (ascii-non-control? char) (char<=? #\space char #\~))
+  (string-for-each (lambda (char)
+                     (unless (ascii-non-control? char)
+                       (error "Non-ASCII character:" char)))
+                   string))
 
 (define (bibtex-line->lose-forms line)
   (cond ((rxmatch #/^@([a-z]+)\{([A-Za-z0-9]+),$/ line)
@@ -14,6 +23,7 @@
          => (lambda (m)
               (let ((field (string->symbol (rxmatch-substring m 1)))
                     (value (rxmatch-substring m 2)))
+                (assert-ascii value)
                 (case field
                   ((author)
                    (map (lambda (author) `(author ,author))
@@ -25,8 +35,34 @@
         (else
          (error "Cannot parse BibTeX line:" line))))
 
+(define (all-whitespace->spaces string)
+  (string-trim-both (regexp-replace-all #/\s+/ string " ")))
+
+(define (all-chars->ascii-graphic str)
+  (let ((control-L #\x0C))
+    (set! str
+      (regexp-replace-all (regexp-quote (string control-L)) str "fi"))
+    (assert-ascii str)
+    str))
+
+(define (bibtex-lines+abstract->lose-forms lines)
+  (let bibtex ((lines lines) (all-forms '()))
+    (let ((new-forms (bibtex-line->lose-forms (car lines))))
+      (if (not (null? new-forms))
+          (bibtex (cdr lines) (append all-forms new-forms))
+          (let ((abstract
+                 (all-chars->ascii-graphic
+                  (all-whitespace->spaces
+                   (string-join (cdr lines) " ")))))
+            (append all-forms
+                    `((pdf "")
+                      ,@(if (string-null? abstract) '()
+                            `((abstract ,abstract))))))))))
+
 (define (writeln x) (write x) (newline))
 
-(define (main . args)
+(define (main)
   (newline)
-  (for-each writeln (append-map bibtex-line->lose-forms (read-all-lines))))
+  (for-each writeln (bibtex-lines+abstract->lose-forms (read-all-lines))))
+
+(main)
