2728665f4c8bade646ca93f7e9d0ca7a9ccc2fc3
Lassi Kortela <lassi@lassi.io>
2020-03-04 12:22:27 +0200

    Spin off LOSE bibliography reader library

---
+++ tools/lose-bib-read.sld
@@ -0,0 +1,35 @@
+(define-library (lose-bib-read)
+  (export lose-bib-read-all lose-bib-read-file)
+  (import (scheme base) (scheme file) (scheme read))
+  (begin
+
+    (define (read-group-loop form group)
+      (cond ((eof-object? form)
+             (error "Missing end-group"))
+            ((equal? 'end-group (car form))
+             (unless (null? (cdr form)) (error "end-group: usage"))
+             (reverse group))
+            ((equal? 'group (car form))
+             (let ((inner-group (read-group form)))
+               (read-group-loop (read) (cons inner-group group))))
+            ((equal? 'id (car form))
+             (let read-entry ((form (read)) (entry (list form)))
+               (if (or (eof-object? form)
+                       (member (car form) '(id group end-group)))
+                   (read-group-loop form (cons (reverse entry) group))
+                   (read-entry (read) (cons form entry)))))
+            (else
+             (error "Expecting (id ...)"))))
+
+    (define (read-group form)
+      (unless (= 1 (length (cdr form))) (error "group: usage"))
+      (let ((group-name (cadr form)))
+        (read-group-loop (read) (list group-name))))
+
+    (define (lose-bib-read-all)
+      (let ((top-group (read-group (read))))
+        (unless (eof-object? (read)) (error "Things after end-group"))
+        top-group))
+
+    (define (lose-bib-read-file filename)
+      (with-input-from-file filename lose-bib-read-all))))
--- tools/pp.scm
+++ tools/pp.scm
@@ -1,6 +1,7 @@
 #!
 
-(import (scheme base) (scheme read) (scheme write))
+(import (scheme base) (scheme write))
+(import (lose-bib-read))
 
 (define (disp . xs) (for-each display xs) (newline))
 
@@ -59,33 +60,6 @@
     ((12) "December")
     (else (error "Bad month:" month))))
 
-(define (read-group-loop form group)
-  (cond ((eof-object? form)
-         (error "Missing end-group"))
-        ((equal? 'end-group (car form))
-         (unless (null? (cdr form)) (error "end-group: usage"))
-         (reverse group))
-        ((equal? 'group (car form))
-         (let ((inner-group (read-group form)))
-           (read-group-loop (read) (cons inner-group group))))
-        ((equal? 'id (car form))
-         (let read-entry ((form (read)) (entry (list form)))
-           (if (or (eof-object? form) (member (car form) '(id group end-group)))
-               (read-group-loop form (cons (reverse entry) group))
-               (read-entry (read) (cons form entry)))))
-        (else
-         (error "Expecting (id ...)"))))
-
-(define (read-group form)
-  (unless (= 1 (length (cdr form))) (error "group: usage"))
-  (let ((group-name (cadr form)))
-    (read-group-loop (read) (list group-name))))
-
-(define (read-all-entries)
-  (let ((top-group (read-group (read))))
-    (unless (eof-object? (read)) (error "Things after end-group"))
-    top-group))
-
 (define (italic text) (string-append "_" text "_"))
 (define (quoted text) (string-append "\"" text "\""))
 (define (linkto url text) (string-append "[" text "](" url ")"))
@@ -119,6 +93,4 @@
   (for-each (lambda (entry) (for-each writeln entry) (newline))
             entries))
 
-;;(display-entries-as-markdown (read-all-entries))
-;;(redisplay-entries-as-lose (read-all-entries))
-(write (read-all-entries)) (newline)
+(writeln (lose-bib-read-all))
